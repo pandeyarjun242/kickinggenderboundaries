5a61bd1515d49974a5cc477446a00c31ea23aebc
arjunpandey2003 <arjunpandey2003@gmail.com>
2020-01-18 11:43:09 +0530

    Admin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Locations, Events
+from .models import Locations, Events, Response
 
 # Register your models here.
 admin.site.register(Locations)
 admin.site.register(Events)
+admin.site.register(Response)
